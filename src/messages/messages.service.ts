9bacd25c48abed630d02fe2e0dfe85c8a4599a6d
Vanessa <vanvan.nellyn@gmail.com>
2023-08-11 11:40:11 +0800

    Added service and repository files

---
+++ src/messages/messages.service.ts
@@ -0,0 +1,16 @@
+import { readFile, writeFile } from "fs"
+
+export class MessagesService {
+
+  async findOne(id: string) {
+
+  }
+
+  async findAll() {
+
+  }
+
+  async create(message:string) {
+
+  }
+}

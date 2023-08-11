e186cc6eb3792c930c673d1dff63137d1b4b8b24
Vanessa <vanvan.nellyn@gmail.com>
2023-08-11 10:58:07 +0800

    Added route handlers and REST Client

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+
+  @Get() 
+  listMessages() {
+
+  }
+
+  @Post()
+  createMessage(@Body() body: any) {
+    console.log(body)
+  }
+
+  @Get('/:id')
+  getMessage(@Param() id: string) {
+    console.log(id)
+  }
+}
